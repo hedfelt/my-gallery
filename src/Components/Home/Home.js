a4651e386b2ba56b53c5decac2cdc345967a7716
Hanne  Edfelt <hanne.edfelt@gmail.com>
2021-06-16 19:36:42 +0200

    gsap continued

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -21,37 +21,62 @@ const Home = () => {
 
   gsap.registerPlugin(ScrollTrigger);
   gsap.core.globals("ScrollTrigger", ScrollTrigger);
-
-  useEffect(() => {
-    gsap.from(".home__art", {
-      scrollTrigger: {
-        trigger: ".home__art",
-        scrub: true,
-        // scrub: 0.5,
-        start: "top bottom",
-        end: "top top",
-      },
-      y: 200,
-      transformOrigin: "center",
-      ease: "power1",
-    });
-  }, [postData]);
+  const ref = useRef(null);
+  const refSix = useRef(null);
 
   useEffect(() => {
     gsap.from(".home__duoLeft", {
       scrollTrigger: {
-        trigger: ".home__duoLeft",
-        // scrub: true,
-        scrub: 0.5,
+        trigger: ref.current,
+        scrub: true,
         start: "top bottom",
         end: "top top",
       },
-      scale: 2,
-
-      transformOrigin: "center",
+      scale: 1.5,
       ease: "power1",
     });
-  }, [postData]);
+    gsap.from(".home__duoRight", {
+      scrollTrigger: {
+        trigger: ".home__duoRight",
+        scrub: true,
+        start: "top bottom",
+        end: "top top",
+      },
+      scale: 1.5,
+      ease: "power1",
+    });
+    gsap.from(".home__wrapperFive", {
+      scrollTrigger: {
+        trigger: ".home__wrapperFive",
+        scrub: true,
+        start: "top bottom",
+        end: "top top",
+      },
+      y: 200,
+      ease: "power1",
+    });
+
+    gsap.from(".home__imageFive", {
+      scrollTrigger: {
+        trigger: ".home__imageFive",
+        scrub: true,
+        start: "top bottom",
+        end: "top top",
+      },
+      scale: 1.2,
+      ease: "power1",
+    });
+    gsap.from(".home__imageSix", {
+      scrollTrigger: {
+        trigger: refSix.current,
+        scrub: true,
+        start: "top bottom",
+        end: "top top",
+      },
+      scale: 1.2,
+      ease: "power1",
+    });
+  }, [postData, ref, refSix]);
 
   const builder = imageUrlBuilder(sanityClient);
 
@@ -88,28 +113,28 @@ const Home = () => {
         <div className="home__see">& See</div>
       </div>
 
-      <motion.div className="home__textgroup">
-        <motion.div
+      <div className="home__textgroup">
+        <div
           className="home__hanne"
-          initial={{ y: "15rem", opacity: 0 }}
-          animate={{ y: 0, opacity: 1 }}
-          transition={{ duration: 0.8 }}
+          // initial={{ y: "15rem", opacity: 0 }}
+          // animate={{ y: 0, opacity: 1 }}
+          // transition={{ duration: 0.8 }}
         >
           HANNE
-        </motion.div>
+        </div>
         <div className="home__verticalTwo">
           <div className="home__beholdTwo">Behold</div>
           <div className="home__seeTwo">& See</div>
         </div>
-        <motion.div
+        <div
           className="home__edfelt"
-          initial={{ y: "15rem", opacity: 0 }}
-          animate={{ y: 0, opacity: 1 }}
-          transition={{ duration: 1 }}
+          // initial={{ y: "15rem", opacity: 0 }}
+          // animate={{ y: 0, opacity: 1 }}
+          // transition={{ duration: 1 }}
         >
           {/* EDFELT */}
           <CurvedText />
-        </motion.div>
+        </div>
 
         <motion.div
           className="home__portfolio"
@@ -123,72 +148,80 @@ const Home = () => {
           <Art />
         </div>
         <div className="home__watercolors">Watercolors</div>
-      </motion.div>
+      </div>
 
-      <div className="frontart">
-        <div className="home__duo">
-          <div className="home__wrapper">
-            <div className="home__container">
-              <img
-                src={urlFor(postData[1].mainImage).width(1000).height(1200)}
-                alt={postData[1].mainImage.alt}
-                className="home__duoLeft"
-              />
-            </div>
-            {postData[1].title} <br /> Watercolor and pen on paper
+      <div className="home__duo">
+        <div className="home__wrapper">
+          <div className="home__container">
+            <img
+              ref={ref}
+              src={urlFor(postData[1].mainImage).width(1000).height(1200)}
+              alt={postData[1].mainImage.alt}
+              className="home__duoLeft"
+            />
           </div>
-          <div className="home__wrapper">
-            <div className="home__container">
-              <img
-                src={urlFor(postData[3].mainImage).width(1000).height(1200)}
-                alt={postData[3].mainImage.alt}
-                className="home__duoRight"
-              />
-            </div>
-            {postData[3].title} <br /> Watercolor and pen on paper
+          {postData[1].title} <br /> Watercolor and pen on paper
+        </div>
+        <div className="home__wrapper">
+          <div className="home__container">
+            <img
+              src={urlFor(postData[3].mainImage).width(1000).height(1200)}
+              alt={postData[3].mainImage.alt}
+              className="home__duoRight"
+            />
           </div>
-        </div>
-      </div>
-      <div className="home__wrapperFour">
-        <div className="home__empty"></div>
-        <div className="home__containerFour">
-          <img
-            src={urlFor(postData[0].mainImage).width(1400).height(1600)}
-            alt={postData[0].mainImage.alt}
-            className="home__imageFour"
-          />
-        </div>
-        <div className="home__titleFour">
-          {postData[0].title} <br /> Watercolor on paper
-        </div>
-      </div>
-      {/* /* IMAGE 2 * smaller image square, fit inside wp, faster scroll + scale out/ */}
-      <div className="home__wrapperFive">
-        <div className="home__containerFive">
-          <img
-            src={urlFor(postData[5].mainImage).width(1000).height(1200)}
-            alt={postData[5].mainImage.alt}
-            className="home__imageFive"
-          />
-        </div>
-        <div className="home__title">
-          {postData[5].title} <br /> Watercolor on paper
+          {postData[3].title} <br /> Watercolor and pen on paper
         </div>
       </div>
 
-      <div className="home__outerwrapperLeft">
+      <div className="home__lorem">
+        Lorem ipsum <br /> sit amet consectetur adipisicing elit.
+        <br />
+        Rerum, libero!
+      </div>
+
+      {/* //grid */}
+      <div className="home__grid">
+        <div className="home__wrapperFour">
+          <div className="home__containerFour">
+            <img
+              src={urlFor(postData[0].mainImage).width(1400).height(1600)}
+              alt={postData[0].mainImage.alt}
+              className="home__imageFour"
+            />
+          </div>
+          <div className="home__titleFour">
+            {postData[0].title} <br /> Watercolor on paper
+          </div>
+        </div>
+
+        <div className="home__wrapperFive">
+          <div className="home__containerFive">
+            <img
+              src={urlFor(postData[5].mainImage).width(1000).height(1200)}
+              alt={postData[5].mainImage.alt}
+              className="home__imageFive"
+            />
+          </div>
+          <div className="home__title">
+            {postData[5].title} <br /> Watercolor on paper
+          </div>
+        </div>
+
         <div className="home__wrapperSix">
           <div className="home__containerSix">
             <img
               src={urlFor(postData[2].mainImage).width(1200).height(1400)}
               alt={postData[2].mainImage.alt}
               className="home__imageSix"
+              ref={refSix}
             />
           </div>
           <div className="home__title">
             {postData[2].title} <br /> Watercolor on paper
           </div>
         </div>
+        {/* </div> */}
       </div>
     </div>
   );
