10433cdc85ea946cd0a1882770c41304fcacd385
Hanne  Edfelt <hanne.edfelt@gmail.com>
2021-06-17 14:45:55 +0200

    added gsap in new component

--- src/Components/FrontArt/FrontArt.js
+++ src/Components/FrontArt/FrontArt.js
@@ -8,6 +8,8 @@ import {
   useSpring,
   useElementScroll,
 } from "framer-motion";
+import sanityClient from "../../client.js";
+import imageUrlBuilder from "@sanity/image-url";
 
 import { Link } from "react-router-dom";
 import { useInView } from "react-intersection-observer";
@@ -15,74 +17,60 @@ import LocomotiveScroll from "locomotive-scroll";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-// const FrontArt = () => {
-//   gsap.registerPlugin(ScrollTrigger);
-//   gsap.registerPlugin(ScrollTrigger);
+export const FrontArt = (props) => {
+  gsap.registerPlugin(ScrollTrigger);
+  const ref = useRef(null);
+  const builder = imageUrlBuilder(sanityClient);
 
-//   const scaleRef = useRef();
-//   const scaleRefTwo = useRef();
+  function urlFor(source) {
+    return builder.image(source);
+  }
 
-//   useEffect(() => {
-//     gsap.from(".frontart__duoImageOne", {
-//       scrollTrigger: {
-//         trigger: scaleRef.current,
-//         scrub: 0.5,
-//         start: "bottom bottom",
-//         end: "top top",
-//       },
-//       scale: 2,
-//       transformOrigin: "center",
-//       ease: "none",
-//     });
-//   }, []);
+  useEffect(
+    () => {
+      gsap.to(ref.current, {
+        scrollTrigger: {
+          trigger: ref.current,
+          scrub: true,
+          start: "top bottom",
+          end: "top top",
+        },
+        scale: 0.9,
+        y: props.scroll,
+        ease: "power1",
+      });
+    },
 
-//   useEffect(() => {
-//     gsap.from(".frontart__duoImageTwo", {
-//       scrollTrigger: {
-//         trigger: ".frontart__duoImageTwo",
-//         scrub: 0.5,
-//         start: "bottom bottom",
-//         end: "top top",
-//       },
-//       scale: 2,
-//       transformOrigin: "center",
-//       ease: "none",
-//     });
-//   }, []);
+    [props.postData],
+    ref
+  );
 
-//   const animation = useAnimation();
-//   const [contentRef, inView] = useInView({
-//     rootMargin: "-500px",
-//   });
+  let scrollRef = 0;
 
-//   useEffect(() => {
-//     if (inView) {
-//       animation.start("visible");
-//     } else {
-//       animation.start("hidden");
-//     }
-//   }, [animation, inView]);
+  useEffect(() => {
+    window.addEventListener(
+      "scroll",
+      function () {
+        // increase value up to 10, then refresh AOS
+        scrollRef <= 1000 ? scrollRef++ : ScrollTrigger.refresh();
+      },
 
-//   const animationtwo = useAnimation();
+      { once: true }
+    );
+  }, []);
 
-//   const [contentReftwo, inViewtwo] = useInView({
-//     rootMargin: "-300px",
-//   });
+  return (
+    <div ref={ref}>
+      <div className="frontart__container">
+        <img
+          src={urlFor(props.postData.mainImage).width(1000).height(1200)}
+          alt={props.postData.mainImage.alt}
+          className="frontart__image"
+        />
+      </div>
+      {props.postData.title} <br /> Watercolor and pen on paper
+    </div>
+  );
+};
 
-//   useEffect(() => {
-//     if (inViewtwo) {
-//       animationtwo.start("visible");
-//     }
-//   }, [animationtwo, inViewtwo]);
-
-//   const animationthree = useAnimation();
-
-//   const [contentRefthree, inViewthree] = useInView({
-//     rootMargin: "-100px",
-//   });
-
-//   useEffect(() => {
-//     if (inViewthree) {
-//       animationthree.start("visible");
-//     }
-//   }, [animationthree, inViewthree]);
+export default FrontArt;
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef, useState } from "react";
 import "./Home.scss";
 import { motion, useViewportScroll, useTransform } from "framer-motion";
-// import FrontArt from "../FrontArt/FrontArt";
+import FrontArt from "../FrontArt/FrontArt";
 import PinkLine from "../../UI/PinkLine/PinkLine";
 import Line from "../../UI/Line/Line";
 import Art from "../../UI/Art/Art";
@@ -19,75 +19,6 @@ import CurvedText from "../../UI/CurvedText/CurvedText";
 
 const Home = () => {
   const [postData, setPost] = useState(null);
-
-  gsap.registerPlugin(ScrollTrigger);
-  const ref = useRef(null);
-  const refSix = useRef(null);
-  const refSixImage = useRef(null);
-
-  useEffect(() => {
-    // gsap.from(".home__duoLeft", {
-    //   scrollTrigger: {
-    //     trigger: ref.current,
-    //     scrub: true,
-    //     start: "top bottom",
-    //     end: "top top",
-    //   },
-    //   scale: 1.5,
-    //   ease: "power1",
-    // });
-    // gsap.from(".home__duoRight", {
-    //   scrollTrigger: {
-    //     trigger: ".home__duoRight",
-    //     scrub: true,
-    //     start: "top bottom",
-    //     end: "top top",
-    //   },
-    //   scale: 1.5,
-    //   ease: "power1",
-    // });
-    // gsap.from(".home__lorem", {
-    //   scrollTrigger: {
-    //     trigger: ".home__lorem",
-    //     scrub: true,
-    //     start: "top bottom",
-    //     end: "top top",
-    //   },
-    //   scale: 1.5,
-    //   ease: "power1",
-    // });
-    // gsap.from(".home__wrapperFive", {
-    //   scrollTrigger: {
-    //     trigger: ".home__wrapperFive",
-    //     scrub: true,
-    //     start: "top bottom",
-    //     end: "top top",
-    //   },
-    //   y: 200,
-    //   ease: "power1",
-    // });
-
-    // gsap.from(".home__imageFive", {
-    //   scrollTrigger: {
-    //     trigger: ".home__imageFive",
-    //     scrub: true,
-    //     start: "top bottom",
-    //     end: "top top",
-    //   },
-    //   scale: 1.2,
-    //   ease: "power1",
-    // });
-    gsap.to(refSixImage.current, {
-      scrollTrigger: {
-        trigger: refSix.current,
-        scrub: true,
-        start: "top bottom",
-        end: "top top",
-      },
-      scale: 1.2,
-      ease: "power1",
-    });
-  }, [postData]);
 
   const builder = imageUrlBuilder(sanityClient);
 
@@ -118,35 +49,11 @@ const Home = () => {
 
   return (
     <div className="home">
-      {/* <img className="home__image" /> */}
-      <div className="home__vertical">
-        <div className="home__behold">Behold</div>
-        <div className="home__see">& See</div>
-      </div>
-
       <div className="home__textgroup">
-        <div
-          className="home__hanne"
-          // initial={{ y: "15rem", opacity: 0 }}
-          // animate={{ y: 0, opacity: 1 }}
-          // transition={{ duration: 0.8 }}
-        >
-          HANNE
-        </div>
-        <div className="home__verticalTwo">
-          <div className="home__beholdTwo">Behold</div>
-          <div className="home__seeTwo">& See</div>
-        </div>
-        <div
-          className="home__edfelt"
-          // initial={{ y: "15rem", opacity: 0 }}
-          // animate={{ y: 0, opacity: 1 }}
-          // transition={{ duration: 1 }}
-        >
-          {/* EDFELT */}
+        <div className="home__hanne">HANNE</div>
+        <div className="home__edfelt">
           <CurvedText />
         </div>
-
         <motion.div
           className="home__portfolio"
           initial={{ y: "15rem", opacity: 0 }}
@@ -162,7 +69,7 @@ const Home = () => {
       </div>
 
       <div className="home__duo">
-        <div className="home__wrapper">
+        {/* <div className="home__wrapper">
           <div className="home__container">
             <img
               ref={ref}
@@ -170,18 +77,14 @@ const Home = () => {
               alt={postData[1].mainImage.alt}
               className="home__duoLeft"
             />
-          </div>
           {postData[1].title} <br /> Watercolor and pen on paper
+        </div> */}
+        <div className="frontart__wrapper">
+          <FrontArt postData={postData[1]} />
         </div>
-        <div className="home__wrapper">
-          <div className="home__container">
-            <img
-              src={urlFor(postData[3].mainImage).width(1000).height(1200)}
-              alt={postData[3].mainImage.alt}
-              className="home__duoRight"
-            />
-          </div>
-          {postData[3].title} <br /> Watercolor and pen on paper
+
+        <div className="frontart__wrapper">
+          <FrontArt postData={postData[3]} />
         </div>
       </div>
 
@@ -193,7 +96,7 @@ const Home = () => {
 
       {/* //grid */}
       <div className="home__grid">
-        <div className="home__wrapperFour">
+        {/* <div className="home__wrapperFour">
           <div className="home__containerFour">
             <img
               src={urlFor(postData[0].mainImage).width(1400).height(1600)}
@@ -204,8 +107,12 @@ const Home = () => {
           <div className="home__titleFour">
             {postData[0].title} <br /> Watercolor on paper
           </div>
+        </div> */}
+
+        <div className="home__wrapperFour">
+          <FrontArt postData={postData[0]} />
         </div>
-        <div className="home__wrapperFive">
+        {/* <div className="home__wrapperFive">
           <div className="home__containerFive">
             <img
               src={urlFor(postData[5].mainImage).width(1000).height(1200)}
@@ -216,12 +123,18 @@ const Home = () => {
           <div className="home__title">
             {postData[5].title} <br /> Watercolor on paper
           </div>
+        </div> */}
+
+        <div className="home__wrapperFive">
+          <FrontArt postData={postData[5]} scroll={-200} />
+        </div>
+        <div className="home__wrapperSix">
+          <FrontArt postData={postData[2]} />
         </div>
 
-        <div className="home__wrapperSix" ref={refSix}>
+        {/* <div className="home__wrapperSix">
           <div className="home__containerSix">
             <img
-              ref={refSixImage}
               src={urlFor(postData[2].mainImage).width(1200).height(1400)}
               alt={postData[2].mainImage.alt}
               className="home__imageSix"
@@ -230,7 +143,7 @@ const Home = () => {
           <div className="home__title">
             {postData[2].title} <br /> Watercolor on paper
           </div>
-        </div>
+        </div> */}
         {/* </div> */}
       </div>
     </div>
